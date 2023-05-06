4f0ab45d4853b6ac05add2589432dd0568826e2c
chenyw8 <likejava@qq.com>
2023-05-06 17:40:45 +0800

    123

--- chat_interact.py
+++ chat_interact.py
@@ -1,20 +1,11 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
-import sys
-import signal
 
 PRE_TRAINED_MODEL_PATH = "../chatglm-6b"
 
 
-# 退出应用
-def app_exit(signum, frame):
-    sys.exit()
-
-
 # 程序入口
 def main():
-    signal.signal(signal.SIGINT, app_exit)
-    signal.signal(signal.SIGTERM, app_exit)
     tokenizer = AutoTokenizer.from_pretrained(PRE_TRAINED_MODEL_PATH, trust_remote_code=True)
     model = AutoModel.from_pretrained(PRE_TRAINED_MODEL_PATH, trust_remote_code=True).cuda()
     model.to("cuda")
