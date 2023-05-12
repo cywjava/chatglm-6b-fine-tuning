49ed31b63cd02397f3bc13cc8e03fd55986fed7b
chenyw8 <likejava@qq.com>
2023-05-12 11:58:53 +0800

    123

--- bids_book_test.py
+++ bids_book_test.py
@@ -5,8 +5,9 @@ from bids_book_industry_server import find_response_items
 dbutil = DBUtil('192.168.10.30', '12277', 'userapp', '1Qaz2Wsx', 'ai')
 dbutil.print_db_info()
 
+
 def test_bik():
-    question_list = dbutil.query_question_by_sqid("bids_book_20230511140114", "chenyw8", "12")
+    question_list = dbutil.query_question_by_sqid("bids_book_20230511212152", "chenyw8", "8")
     answer1 = question_list[1]
     print(answer1)
     content_items = find_response_items(answer1)
