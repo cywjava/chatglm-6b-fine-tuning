e84a1cc996df1dfb1e91d92e1939e94821226938
chenyw8 <likejava@qq.com>
2023-05-23 15:08:41 +0800

    123

--- start_train.sh
+++ start_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+export CUDA_VISIBLE_DEVICES="3"
 cd /home/train/chatglm-6b-fine-tuning
-
-export CUDA_VISIBLE_DEVICES="0,1"
-nohup python3 ./fine-tuning/fine_tuning_chatglm6b.py --model_path /home/train/model/ --dataset_path "/home/train/data/*" --check_points_path /home/train/check_points/ --train_batch_size 4 >>./logs/train.log 2>&1 &
+rm -rf logs/*.log
+nohup python3 -u ./finetuning/fine_tuning_chatglm6b.py --model_path /home/train/new_model/ --dataset_path "/home/train/data/*" --check_points_path /home/train/check_points/ --train_batch_size 3 --epochs 1 --do_eval --fp16 --fp16_opt_level O2 >> ./logs/train.log 2>&1 &
